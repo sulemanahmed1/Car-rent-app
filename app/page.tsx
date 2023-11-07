c7c6dfdc0df73f4418f5f53fd13ed5c0813add31
suleman ahmed <ahmedsuleman@outlook.dk>
2023-11-07 22:45:03 +0100

    added search manufacturer components and searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
@@ -6,7 +6,22 @@ export default function Home() {
     <main className="overflow-hidden">
       <Hero />
       <div className='mt-12 padding-x padding-y max-width' id="discover">
-        <h1 className=''> </h1>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+          <p>Explore the cars you like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+        <div className='home__filter-container'>
+          <CustomFilter title="fuel" />
+          <CustomFilter title="year" /> 
+        </div>
+
+        </div>
       </div>
     </main>
   )
