d626889d9ed96de261e9c26a7a6c7cdc56762cb1
suleman ahmed <ahmedsuleman@outlook.dk>
2023-11-09 09:39:02 +0100

    added component CarDetails and CarCard

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from './Footer';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 
 export {
     Hero,
@@ -13,6 +15,8 @@ export {
     Footer,
     SearchBar,
     CustomFilter,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard,
+    CarDetails
 
 }
--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,34 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string; 
+    title: string;
     containerStyles?: string;
     handleClick?:
-    MouseEventHandler<HTMLButtonElement>; 
-    btnType?: "button" | "submit"; 
-} 
+    MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
+}
 
 export interface SearchManuFacturerProps {
-    manufacturer: string; 
-    setManuFacturer: (manufacturer: string) => void; 
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
 
+}
+
+
+export interface CarProps {
+    city_mpg: number; 
+    class: string;
+    combination_mpg: number; 
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: 1993
 }
